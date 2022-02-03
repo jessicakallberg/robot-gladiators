9a3d8d94f284ffb2fd2eb9875f454586ea993f3b
Jessica Santiago <jessie_13084@yahoo.com>
2022-02-03 18:52:39 -0500

    changed game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,16 @@
-// this creates a function named "fight"
-function fight() {
-    window.alert("the fight has begun!");
-}
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// Note the lack of quotation marks around playerName 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-var name = "your name";
-console.log(name);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
 
-
-
-//fight();
+fight();
