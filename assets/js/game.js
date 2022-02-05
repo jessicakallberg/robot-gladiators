a5b14567d21f2f9f28d77171825757bc6738c85b
Jessica Santiago <jessie_13084@yahoo.com>
2022-02-04 19:28:35 -0500

    working on fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,8 +22,13 @@ console.log(
 );
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
+
 
   // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );  
 };
 
 
